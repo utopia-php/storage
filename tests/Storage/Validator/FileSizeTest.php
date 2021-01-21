64199ed6804c72439fb948536a828a20796faa4e
Damodar Lohani <dlohani48@gmail.com>
2021-01-21 07:54:39 +0545

    storage tests, updated namespace, fixing paths

---
+++ tests/Storage/Validator/FileSizeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Utopia\Tests;
+
+use Utopia\Storage\Validator\FileSize;
+use PHPUnit\Framework\TestCase;
+
+class FileSizeTest extends TestCase
+{
+    /**
+     * @var FileSize
+     */
+    protected $object = null;
+
+    public function setUp(): void
+    {
+        $this->object = new FileSize(1000);
+    }
+
+    public function tearDown(): void
+    {
+    }
+
+    public function testValues()
+    {
+        $this->assertEquals($this->object->isValid(1001), false);
+        $this->assertEquals($this->object->isValid(1000), true);
+        $this->assertEquals($this->object->isValid(999), true);
+    }
+}
