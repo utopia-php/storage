27a9102c3920134f55f16146611fc9656d881e90
Damodar Lohani <dlohani48@gmail.com>
2021-01-21 07:17:34 +0545

    storage copied and namespace updates

---
+++ src/Storage/Validator/FileSize.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Utopia\Storage\Validator;
+
+use Utopia\Validator;
+
+class FileSize extends Validator
+{
+    /**
+     * @var int
+     */
+    protected $max;
+
+    /**
+     * Max size in bytes
+     * 
+     * @param int $max
+     */
+    public function __construct($max)
+    {
+        $this->max = $max;
+    }
+
+    public function getDescription()
+    {
+        return 'File size can\'t be bigger than '.$this->max;
+    }
+
+    /**
+     * Finds whether a file size is smaller than required limit.
+     *
+     * @param mixed $fileSize
+     *
+     * @return bool
+     */
+    public function isValid($fileSize)
+    {
+        if (!is_int($fileSize)) {
+            return false;
+        }
+        
+        if ($fileSize > $this->max) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/Storage/Validator/Upload.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Utopia\Storage\Validator;
+
+use Utopia\Validator;
+
+class Upload extends Validator
+{
+    public function getDescription()
+    {
+        return 'Not a valid upload file';
+    }
+
+    /**
+     * Check if a file is a valid upload file
+     *
+     * @param mixed $path
+     *
+     * @return bool
+     */
+    public function isValid($path)
+    {
+        if (!is_string($path)) {
+            return false;
+        }
+        
+        if (\is_uploaded_file($path)) {
+            return true;
+        }
+        
+        return false;
+    }
+}
