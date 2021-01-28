b940916375373e473a6c93988a49622112145a58
Damodar Lohani <dlohani48@gmail.com>
2021-01-28 15:25:01 +0545

    digitalocean spaces adapter

---
+++ src/Storage/Device/DoSpaces.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Utopia\Storage\Device;
+
+use Utopia\Storage\Device\S3;
+
+class DoSpaces extends S3
+{
+
+    /**
+     * Regions constants
+     *
+     */
+    const SGP1 = 'sgp1';
+    const NYC3 = 'nyc3';
+    const FRA1 = 'fra1';
+    const SFO2 = 'sfo2';
+    const SFO3 = 'sfo3';
+    const AMS3 = 'AMS3';
+
+    protected $endpoint;
+
+    /**
+     * DoSpaces Constructor
+     *
+     * @param string $root
+     * @param string $accessKey
+     * @param string $secretKey
+     * @param string $bucket
+     * @param string $region
+     * @param string $acl
+     */
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::NYC3, string $acl = self::ACL_PRIVATE)
+    {
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+        $this->headers['Host'] = $bucket . '.' . $region . '.digitaloceanspaces.com';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'Digitalocean Spaces Storage';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'Digitalocean Spaces Storage';
+    }
+
+}
