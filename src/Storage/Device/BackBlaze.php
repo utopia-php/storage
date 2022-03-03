0ec0334f162d1f531f2b0ac03b638d3616496616
Everly Precia Suresh <everlyprecia@gmail.com>
2022-03-03 16:41:46 +0000

    Initial Implementation

---
+++ src/Storage/Device/BackBlaze.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Utopia\Storage\Device;
+
+use Utopia\Storage\Device\S3;
+
+class BackBlaze extends S3
+{
+    /**
+     * Regions constants
+     *
+     */
+    const US_WEST_001 = 'us-west-001';
+    const US_WEST_002 = 'us-west-002';
+    const US_WEST_003 = 'us-west-003';
+    const US_WEST_004 = 'us-west-004';
+    const EU_CENTRAL_001 = 'eu-central-001';
+    const EU_CENTRAL_002 = 'eu-central-002';
+    const EU_CENTRAL_003 = 'eu-central-003';
+    const EU_CENTRAL_004 = 'eu-central-004';
+
+    /**
+     * BackBlaze Constructor
+     *
+     * @param string $root
+     * @param string $accessKey
+     * @param string $secretKey
+     * @param string $bucket
+     * @param string $region
+     * @param string $acl
+     */
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::US_WEST_004, string $acl = self::ACL_PRIVATE)
+    {
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+        $this->headers['host'] = $bucket . '.' . 's3' . $region . '.backblazeb2.com';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'BackBlaze B2 Storage';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'BackBlaze B2 Storage';
+    }
+}
---
+++ tests/Storage/Device/BackBlazeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Utopia\Tests;
+
+use Utopia\Storage\Device\BackBlaze;
+use Utopia\Tests\S3Base;
+
+class BackBlazeTest extends S3Base
+{
+    protected function init(): void
+    {
+        $this->root = '/root';
+        $key = $_SERVER['BACKBLAZE_ACCESS_KEY'] ?? '';
+        $secret = $_SERVER['BACKBLAZE'] ?? '';
+        $bucket = "backblaze-demo ";
+
+        $this->object = new BackBlaze($this->root, $key, $secret, $bucket, BackBlaze::US_WEST_004, BackBlaze::ACL_PRIVATE);
+
+    }
+
+    protected function getAdapterName(): string
+    {
+        return 'BackBlaze B2 Storage';
+    }
+
+    protected function getAdapterDescription(): string
+    {
+        return 'BackBlaze B2 Storage';
+    }
+}
