3d4fbf68637e26ceddbd454e7a85f394a5134c37
Shimon Newman <shimonewman@gmail.com>
2022-05-09 09:38:31 +0300

    Update src/Storage/Device/Linode.php
    Co-authored-by: Matej Bačo <matejbaco2000@gmail.com>

--- src/Storage/Device/Linode.php
+++ src/Storage/Device/Linode.php
@@ -29,7 +29,7 @@ class Linode extends Generic
      */
     public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::EU_CENTRAL_1, string $acl = self::ACL_PRIVATE)
     {
-        $hostName = $bucket.'.'.$region.'.'.'linodeobjects.com';
+        $hostName = $bucket.'.'.$region.'.linodeobjects.com';
         parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl, $hostName);
     }
 
