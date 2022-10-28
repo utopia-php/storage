4069fdc9ad158b035d8a5893e7cbb64970cdaeb1
aloks98 <aloksahoo@acceldata.io>
2022-10-29 01:54:09 +0530

    feat: add dreamobjects storage

---
+++ src/Storage/Device/DreamObjects.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Utopia\Storage\Device;
+
+use Utopia\Storage\Device\S3;
+
+class DreamObjects extends S3
+{
+    /**
+     * Regions constants
+     *
+     */
+    const US_EAST_1='us-east-1';
+
+    /**
+     * Object Storage Constructor
+     *
+     * @param string $root
+     * @param string $accessKey
+     * @param string $secretKey
+     * @param string $bucket
+     * @param string $region
+     * @param string $acl
+     */
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::US_EAST_1, string $acl = self::ACL_PRIVATE)
+    {
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+        $this->headers['host'] = $bucket.'objects-'.$region.'.'.'dream.io';
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'DreamHost Object Storage';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return 'DreamHost Object Storage';
+    }
+}
