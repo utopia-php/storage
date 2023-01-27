2e9d1de86d6dbed8dd08b68052b5db46cdcada64
Christy Jacob <christyjacob4@gmail.com>
2023-01-27 13:34:02 +0000

    fix: failing tests

--- tests/Storage/Device/LocalTest.php
+++ tests/Storage/Device/LocalTest.php
@@ -141,28 +141,6 @@ class LocalTest extends TestCase
         $this->assertGreaterThan(0, $this->object->getDirectorySize(__DIR__.'/../../resources/disk-b/'));
     }
 
-    public function testFileMimeType()
-    {
-        $this->assertEquals($this->object->getFileMimeType(__DIR__.'/../../resources/disk-a/kitten-1.jpg'), 'image/jpeg');
-        $this->assertEquals($this->object->getFileMimeType(__DIR__.'/../../resources/disk-a/kitten-2.jpg'), 'image/jpeg');
-        $this->assertEquals($this->object->getFileMimeType(__DIR__.'/../../resources/disk-b/kitten-1.png'), 'image/png');
-        $this->assertEquals($this->object->getFileMimeType(__DIR__.'/../../resources/disk-b/kitten-2.png'), 'image/png');
-    }
-
-    public function testFileHash()
-    {
-        $this->assertEquals($this->object->getFileHash(__DIR__.'/../../resources/disk-a/kitten-1.jpg'), '7551f343143d2e24ab4aaf4624996b6a');
-        $this->assertEquals($this->object->getFileHash(__DIR__.'/../../resources/disk-a/kitten-2.jpg'), '81702fdeef2e55b1a22617bce4951cb5');
-        $this->assertEquals($this->object->getFileHash(__DIR__.'/../../resources/disk-b/kitten-1.png'), '03010f4f02980521a8fd6213b52ec313');
-        $this->assertEquals($this->object->getFileHash(__DIR__.'/../../resources/disk-b/kitten-2.png'), '8a9ed992b77e4b62b10e3a5c8ed72062');
-    }
-
-    public function testDirectorySize()
-    {
-        $this->assertGreaterThan(0, $this->object->getDirectorySize(__DIR__.'/../../resources/disk-a/'));
-        $this->assertGreaterThan(0, $this->object->getDirectorySize(__DIR__.'/../../resources/disk-b/'));
-    }
-
     public function testPartUpload()
     {
         $source = __DIR__.'/../../resources/disk-a/large_file.mp4';
