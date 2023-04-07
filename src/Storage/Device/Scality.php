53cfea223ac462fa362e703ee96654d6e11861cd
Aleksandr Ivanov <misantron@gmail.com>
2023-04-07 21:17:23 +0300

    Pass host as optional constructor parameter

--- src/Storage/Device/Scality.php
+++ src/Storage/Device/Scality.php
@@ -6,9 +6,18 @@ use Utopia\Storage\Storage;
 
 class Scality extends S3
 {
-    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::US_EAST_1, string $acl = self::ACL_PRIVATE)
-    {
+    public function __construct(
+        string $root,
+        string $accessKey,
+        string $secretKey,
+        string $bucket,
+        string $region = self::US_EAST_1,
+        string $acl = self::ACL_PRIVATE,
+        ?string $host = null
+    ) {
         parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+
+        $this->headers['host'] = $host ?? $bucket.'.s3.'.$region.'.amazonaws.com';
     }
 
     /**
