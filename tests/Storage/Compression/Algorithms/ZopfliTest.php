9bcc211c049e71bad2f29bc595c9fb521d100abc
Mohd Toukir Khan <88899011+toukirkhan@users.noreply.github.com>
2023-04-25 23:05:11 +0530

    Create ZopfliTest.php

---
+++ tests/Storage/Compression/Algorithms/ZopfliTest.php
@@ -0,0 +1,41 @@
+@@ -0,0 +1,40 @@
+<?php
+
+namespace Utopia\Storage\Compression\Algorithms;
+
+use Utopia\Storage\Compression\Compression;
+
+class Zopfli extends Compression
+{
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'zopfli';
+    }
+
+    /**
+     * Compress.
+     *
+     * @param string $data
+     *
+     * @return string
+     */
+    public function compress(string $data): string
+    {
+        return \zopfliencode($data);
+    }
+
+    /**
+     * Decompress.
+     *
+     * @param string $data
+     *
+     * @return string
+     */
+    public function decompress(string $data): string
+    {
+        return \zopflidecode($data);
+    }
+}
