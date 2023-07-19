66e8aaf20e9b068f24bdf1195d3b6fb98427a17c
jamesrcramos <76936793+jamesrcramos@users.noreply.github.com>
2023-07-19 20:09:15 +0000

    Uncommented important construct call

--- src/Storage/Device/AzureBlob.php
+++ src/Storage/Device/AzureBlob.php
@@ -18,7 +18,7 @@ class AzureBlob extends S3
     
     public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::EU_CENTRAL_1, string $acl = self::ACL_PRIVATE)   //Needs constructor variables
     {
-        //parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
         $this->headers['host'] = $bucket.'blob.core.windows.net';
     }
 
