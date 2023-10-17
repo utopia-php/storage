856ff23aedcc2f84ba33a9f9f1b3f3b7c626bd4c
basichacker881 <mugdhasharma0327@gmail.com>
2023-10-17 09:50:45 +0530

    adds test fixes

--- src/Storage/Device/Ceph.php
+++ src/Storage/Device/Ceph.php
@@ -2,6 +2,8 @@
 
 namespace Utopia\Storage\Device;
 
+use Utopia\Storage\Storage;
+
 class Ceph extends S3
 {
     /**
@@ -9,37 +11,31 @@ class Ceph extends S3
      */
     const US_EAST_1 = 'us-east-1';
 
+    const US_EAST_2 = 'us-east-2';
+
+    const US_WEST_1 = 'us-west-1';
+
+    const US_WEST_2 = 'us-west-2';
+
     /**
      * Ceph Constructor
-     *
-     * @param  string  $root
-     * @param  string  $accessKey
-     * @param  string  $secretKey
-     * @param  string  $bucket
-     * @param  string  $region
-     * @param  string  $acl
      */
-    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::US_WEST_001, string $acl = self::ACL_PRIVATE)
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $cephHost, string $region = self::US_WEST_001, string $acl = self::ACL_PRIVATE)
     {
         parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
-        $this->headers['host'] = $bucket.'.'.'s3'.'.'.$region.'.ceph.io';
+        $this->headers['host'] = $cephHost;
     }
-    /**
-     * @return string
-     */
+
     public function getName(): string
     {
         return 'Ceph Storage';
     }
 
-    /**
-     * @return string
-     */
     public function getDescription(): string
     {
         return 'Ceph Storage';
     }
-    
+
     public function getType(): string
     {
         return Storage::DEVICE_CEPH;
