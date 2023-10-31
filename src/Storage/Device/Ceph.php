de7bf1cb42bd815b5cbe0c5fb99c093084dd3c85
basichacker881 <mugdhasharma0327@gmail.com>
2023-10-31 22:10:50 +0530

    removes regions

--- src/Storage/Device/Ceph.php
+++ src/Storage/Device/Ceph.php
@@ -7,22 +7,11 @@ use Utopia\Storage\Storage;
 class Ceph extends S3
 {
     /**
-     * Regions constants
-     */
-    const US_EAST_1 = 'us-east-1';
-
-    const US_EAST_2 = 'us-east-2';
-
-    const US_WEST_1 = 'us-west-1';
-
-    const US_WEST_2 = 'us-west-2';
-
-    /**
      * Ceph Constructor
      */
-    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $cephHost, string $region = self::US_WEST_001, string $acl = self::ACL_PRIVATE)
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $cephHost, string $acl = self::ACL_PRIVATE)
     {
-        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $acl);
         $this->headers['host'] = $cephHost;
     }
 
