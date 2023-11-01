eeef840403e90103f2a2d263d4e7c24d97040a51
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-11-01 15:56:33 +0530

    Update Exoscale.php

--- src/Storage/Device/Exoscale.php
+++ src/Storage/Device/Exoscale.php
@@ -11,11 +11,17 @@ class Exoscale extends S3
      * Exoscale Regions constants
      */
     const CH_GVA_2 = 'ch-gva-2';
+    
     const CH_DK_2 = 'ch-dk-2';
+    
     const DE_FRA_1 = 'de-fra-1';
+    
     const DE_MUC_1 = 'de-muc-1';
+    
     const AT_VIE_1 = 'at-vie-1';
+    
     const AT_VIE_2 = 'at-vie-2';
+    
     const BG_SOF_1 = 'bg-sof-1';
 
     /**
@@ -31,12 +37,7 @@ class Exoscale extends S3
     public function __construct(string $root, string $apiKey, string $apiSecret, string $bucket, string $region = self::CH_GVA_2, string $acl = self::ACL_PRIVATE)
     {
         parent::__construct($root, $apiKey, $apiSecret, $bucket, $region, $acl);
-        
-        if (isset($this->regionEndpointMap[$region])) {
-            $this->headers['host'] = $this->regionEndpointMap[$region];
-        } else {
-            throw new \InvalidArgumentException('Invalid or unsupported region');
-        }
+        $this->headers['host'] = $bucket.'.'.$region.'.'.'exo.io';
     }
 
     /**
@@ -44,7 +45,7 @@ class Exoscale extends S3
      */
     public function getName(): string
     {
-        return 'Exoscale Storage';
+        return 'Exoscale Simple Object Storage';
     }
 
     /**
@@ -52,7 +53,7 @@ class Exoscale extends S3
      */
     public function getDescription(): string
     {
-        return 'Exoscale Storage';
+        return 'Exoscale Simple Object Storage';
     }
 
     /**
