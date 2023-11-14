c7cb031e97367149d60b57734c455f9c57c6e595
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-11-14 09:28:11 +0530

    Update Exoscale.php

--- src/Storage/Device/Exoscale.php
+++ src/Storage/Device/Exoscale.php
@@ -8,21 +8,20 @@ class Exoscale extends S3
 {
     /**
      * Exoscale Regions constants
-     * https://community.exoscale.com/documentation/platform/exoscale-datacenter-zones/
      */
-    const CH_GVA_2 = 'ch-gva-2';
-    
-    const CH_DK_2 = 'ch-dk-2';
-    
-    const DE_FRA_1 = 'de-fra-1';
-    
-    const DE_MUC_1 = 'de-muc-1';
-    
-    const AT_VIE_1 = 'at-vie-1';
-    
-    const AT_VIE_2 = 'at-vie-2';
-    
-    const BG_SOF_1 = 'bg-sof-1';
+    public const CH_GVA_2 = 'ch-gva-2';
+
+    public const CH_DK_2 = 'ch-dk-2';
+
+    public const DE_FRA_1 = 'de-fra-1';
+
+    public const DE_MUC_1 = 'de-muc-1';
+
+    public const AT_VIE_1 = 'at-vie-1';
+
+    public const AT_VIE_2 = 'at-vie-2';
+
+    public const BG_SOF_1 = 'bg-sof-1';
 
     /**
      * Exoscale Constructor
@@ -34,9 +33,9 @@ class Exoscale extends S3
      * @param string $region
      * @param string $acl
      */
-    public function __construct(string $root, string $apiKey, string $apiSecret, string $bucket, string $region = self::CH_GVA_2, string $acl = self::ACL_PRIVATE)
+    public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::EU_CENTRAL_1, string $acl = self::ACL_PRIVATE)
     {
-        parent::__construct($root, $apiKey, $apiSecret, $bucket, $region, $acl);
+        parent::__construct($root, $accessKey, $secretKey, $bucket, $region, $acl);
         $this->headers['host'] = $bucket.'.'.$region.'.'.'exo.io';
     }
 
@@ -45,7 +44,7 @@ class Exoscale extends S3
      */
     public function getName(): string
     {
-        return 'Exoscale Simple Object Storage';
+        return 'Exoscale Storage';
     }
 
     /**
@@ -53,7 +52,7 @@ class Exoscale extends S3
      */
     public function getDescription(): string
     {
-        return 'Exoscale Simple Object Storage';
+        return 'Exoscale Storage';
     }
 
     /**
