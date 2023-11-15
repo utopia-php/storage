e758d7734935ee4c511fdf8a9f8d03437b360d09
Atharva Kulkarni <atharvakulkarni172003@gmail.com>
2023-11-15 14:19:03 +0530

    Update Exoscale.php

--- src/Storage/Device/Exoscale.php
+++ src/Storage/Device/Exoscale.php
@@ -26,12 +26,12 @@ class Exoscale extends S3
     /**
      * Exoscale Constructor
      *
-     * @param string $root
-     * @param string $apiKey
-     * @param string $apiSecret
-     * @param string $bucket
-     * @param string $region
-     * @param string $acl
+     * @param  string  $root
+     * @param  string  $apiKey
+     * @param  string  $apiSecret
+     * @param  string  $bucket
+     * @param  string  $region
+     * @param  string  $acl
      */
     public function __construct(string $root, string $accessKey, string $secretKey, string $bucket, string $region = self::EU_CENTRAL_1, string $acl = self::ACL_PRIVATE)
     {
