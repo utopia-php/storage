9c5de48b6b48bc7ce79b0367d285c10cb62ef275
Damodar Lohani <dlohani48@gmail.com>
2023-12-31 11:23:45 +0000

    fix formatting

--- src/Storage/Compression/Compression.php
+++ src/Storage/Compression/Compression.php
@@ -5,10 +5,15 @@ namespace Utopia\Storage\Compression;
 abstract class Compression
 {
     public const ZSTD = 'zstd';
+
     public const GZIP = 'gzip';
+
     public const BROTLI = 'brotli';
+
     public const LZ4 = 'lz4';
+
     public const SNAPPY = 'snappy';
+
     public const XZ = 'xz';
 
     /**
