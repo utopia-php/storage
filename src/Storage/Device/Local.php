e059b2868b33b53863aca02f19cf973b4e1ff8e6
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-09-12 19:16:48 +0530

    fix test

--- src/Storage/Device/Local.php
+++ src/Storage/Device/Local.php
@@ -156,7 +156,9 @@ class Local extends Device
             : new \DirectoryIterator($source);
 
         foreach ($iterator as $file) {
-            if ($file->isDot()) {
+            // Skip dot files/directories for DirectoryIterator (non-recursive case)
+            // RecursiveDirectoryIterator already has SKIP_DOTS flag set
+            if (! $recursive && $file instanceof \DirectoryIterator && $file->isDot()) {
                 continue;
             }
 
