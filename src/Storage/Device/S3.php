6c8a5925606b141b2262642186575d157c4eee48
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-10-01 17:14:58 +0530

    fix: prefix

--- src/Storage/Device/S3.php
+++ src/Storage/Device/S3.php
@@ -630,10 +630,12 @@ class S3 extends Device
         } catch (\Throwable $th) {
             try {
                 $prefix = $this->getRoot().'/'.ltrim($path, '/');
+                
                 if (! empty($path) && ! str_ends_with($prefix, '/')) {
                     $prefix .= '/';
                 }
 
+                $prefix = ltrim($prefix, '/');
                 $objects = $this->listObjects($prefix, 1);
                 $count = (int) ($objects['KeyCount'] ?? 0);
 
