b8af52e8ae1f9e670b32255a8936bbb46634b711
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2026-06-03 10:51:46 +0530

    Defer storage operation telemetry instrument (#167)

--- tests/Storage/StorageTest.php
+++ tests/Storage/StorageTest.php
@@ -5,7 +5,9 @@ namespace Utopia\Tests\Storage;
 use Exception;
 use PHPUnit\Framework\TestCase;
 use Utopia\Storage\Device\Local;
+use Utopia\Storage\Device\Telemetry;
 use Utopia\Storage\Storage;
+use Utopia\Telemetry\Adapter\Test as TestTelemetry;
 
 Storage::setDevice('disk-a', new Local(__DIR__.'/../resources/disk-a'));
 Storage::setDevice('disk-b', new Local(__DIR__.'/../resources/disk-b'));
@@ -41,5 +43,20 @@ class StorageTest extends TestCase
         $file = '/kitten-1.jpg';
         $device = Storage::getDevice('disk-a');
         $this->assertFalse($device->move($file, $file));
+    }
+
+    public function testStorageOperationTelemetryIsCreatedOnFirstRecord(): void
+    {
+        $telemetry = new TestTelemetry();
+        $underlying = new Local(__DIR__.'/../resources/disk-a');
+        $device = new Telemetry($telemetry, $underlying);
+        $path = $underlying->getPath('lorem.txt');
+
+        $this->assertArrayNotHasKey('storage.operation', $telemetry->histograms);
+
+        $device->exists($path);
+
+        $this->assertArrayHasKey('storage.operation', $telemetry->histograms);
+        $this->assertCount(1, $telemetry->histograms['storage.operation']->values);
     }
 }
